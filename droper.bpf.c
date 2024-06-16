caf88a1e84658c13720bbb46f7244893428e6080
toastsandwich <shreyasxcore2003@gmail.com>
2024-06-16 17:17:19 +0530

    change to count tcp packets

--- droper.bpf.c
+++ droper.bpf.c
@@ -1,42 +1,47 @@
 //go:build ignore
 
 #include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
-#include <bpf/bpf_endian.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-#include <linux/tcp.h>
-#include <arpa/inet.h>
-#include <sys/cdefs.h>
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
 
-#define __TCP 6
+#define _TCP 6
 
-static __always_inline char lookup_protocol(struct xdp_md *ctx) {
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __type(key, __u32);
+    __type(value, __u64);
+    __uint(max_entries, 1);
+} tcp_pkt_count_t SEC(".maps");
+
+static __always_inline unsigned char lookup_pkt_tcp(struct xdp_md *ctx) {
     unsigned char protocol = 0;
-
-    void *data = (void *)(long)ctx->data;
-    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void*)(long) ctx->data;
+    void *data_end = (void*)(long) ctx->data_end;
 
     struct ethhdr *eth = data;
     if(data + sizeof(struct ethhdr) > data_end)
         return 0;
-
+    
     if(bpf_ntohs(eth->h_proto) == ETH_P_IP) {
         struct iphdr *iph = data + sizeof(struct ethhdr);
-        if(data + sizeof(struct ethhdr) + sizeof(struct iphdr) <= data_end)
+        if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) <= data_end)
             protocol = iph->protocol;
     }
     return protocol;
 }
 
 SEC("xdp")
-int xdp_drop_tcp_func(struct xdp_md *ctx) {
-    long protocol = lookup_protocol(ctx);
-    if(protocol == __TCP) {
-        bpf_printk("i see you tcp, lets find this port");
-        // return XDP_DROP;
+int count_tcp_packets(struct xdp_md *ctx) {
+    __u32 key = 0;
+    __u64 *count = bpf_map_lookup_elem(&tcp_pkt_count_t, &key);
+    __u64 protocol = lookup_pkt_tcp(ctx);
+    if (count && protocol == _TCP) {
+        bpf_printk("tcp packet found count will increase");
+        __sync_fetch_and_add(count, 1);
     }
     return XDP_PASS;
 }
 
-char LICENSE[] SEC("license") = "Dual BSD/GPL";
+char LICENSE[] SEC("license") = "Dual MIT/GPL";
