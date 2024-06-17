96496dfeb30f568d0d45b7431bf84f84401eb5cc
toastsandwich <shreyasxcore2003@gmail.com>
2024-06-17 09:55:28 +0530

    added comments in userspace code

--- droper.userspace.go
+++ droper.userspace.go
@@ -11,6 +11,10 @@ import (
 	"github.com/cilium/ebpf/rlimit"
 )
 
+/*
+struct similar to data_t in droper.bpf.c,
+padding to added to make status a multiple of 8 in both codes
+*/
 type Data_t struct {
 	Protocol uint32
 	Port     uint32
@@ -19,22 +23,24 @@ type Data_t struct {
 }
 
 func main() {
+	// Removes MemLock required for kernels with version 5.1 or lesser it is required
 	if err := rlimit.RemoveMemlock(); err != nil {
 		log.Fatal("removing mem lock: ", err)
 	}
 
+	// Load the comiled eBPF ELF and load it to kernel
 	var obj droperObjects
 	if err := loadDroperObjects(&obj, nil); err != nil {
 		log.Fatal("loading object: ", err)
 	}
 	defer obj.Close()
 
-	ifname := "ens33"
+	ifname := "ens33" // <- used virtual machine | else use 'eth0 or wlan0'
 	iface, err := net.InterfaceByName(ifname)
 	if err != nil {
 		log.Fatal("getting interface: ", err)
 	}
-
+	//  Attached DropTcpPackets to network interface
 	link, err := link.AttachXDP(link.XDPOptions{
 		Program:   obj.DropTcpPackets,
 		Interface: iface.Index,
@@ -46,6 +52,7 @@ func main() {
 
 	log.Println("looking for TCP packet(s)")
 
+	// Periodically fetch the Data_t from TcpPktT, exit when iterupted
 	tick := time.Tick(time.Second)
 	stop := make(chan os.Signal, 5)
 	signal.Notify(stop, os.Interrupt)
@@ -56,16 +63,18 @@ func main() {
 			var key uint32 = 0
 			var value Data_t
 			var proto string
-			err := obj.TcpPktT.Lookup(&key, &value)
+			err := obj.TcpPktT.Lookup(&key, &value) //get elem from key
 			if err != nil {
 				log.Println("map lookup: ", err)
 				continue
 			}
+
 			if value.Protocol == 6 {
 				proto = "TCP"
 			} else {
 				proto = "Other than TCP"
 			}
+
 			if value.Port == 4040 {
 				log.Printf("TCP packet caught: Protocol=%s, Port=%d, Status=%s\n", proto, value.Port, string(value.Status[:]))
 			} else {
